7afbe7a3e4258cebe793b5f51b9ebb8276f46959
Josh <63964410+joshhoxy@users.noreply.github.com>
2024-11-26 17:26:34 +0900

    Update HelloworldIT.java

--- src/test-integration/java/com/example/HelloworldIT.java
+++ src/test-integration/java/com/example/HelloworldIT.java
@@ -38,6 +38,6 @@ public class HelloworldIT {
     public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity(base.toString(),
                 String.class);
-        assertThat(response.getBody(), equalTo("Deploy TEST2\n\n"));
+        assertThat(response.getBody(), equalTo("Deploy TEST3\n\n"));
     }
 }
